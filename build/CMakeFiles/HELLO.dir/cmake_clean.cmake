c5aa36339787e14854720b485eefe014bf5e37c6
JVerhe <verherstraeten.jens@gmail.com>
2025-02-19 16:49:44 +0100

    Initialized Cmake

---
+++ build/CMakeFiles/HELLO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HELLO.dir/src/hello.cpp.o"
+  "CMakeFiles/HELLO.dir/src/hello.cpp.o.d"
+  "HELLO"
+  "HELLO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HELLO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
