7321fe385bc5041bbcd655bd89bbce87c9fa59b4
JVerhe <verherstraeten.jens@gmail.com>
2025-02-21 14:14:36 +0100

    Made some changes to the documentation

--- proto/adjoint_method.py
+++ proto/adjoint_method.py
@@ -1,36 +1,28 @@
 import numpy as np
 
-n = 9 # total nodes
-e = 4 # total elements
-K = np.random.rand(n,n)
-F = np.random.rand(n,1)
+# Adjoint method according to ZhangEtAl2007
+def adjoint(T, v, corners, p=1):
+    """
+    Returns a column vector of the derrivative of the const function with respect to the fraction of metal for each element.
+    Args:
+        T (np.ndarray): The global temperature vector during a certain iteration
+        v (np.ndarray): Global vector with fractions of metal in each element
+        corners(dict): Dictionary with the element idx as key, the node indices in a list as value
+        p (float): p > 1 is an float that pushes the fractions of metal to either 0 or 1. Higher values push harder.
 
-km = 65
-kp = 0.2
-p = 1
-K0 = np.matrix([[2/3, -1/6, -1/3, -1/6],[-1/6, 2/3, -1/6, -1/3],[-1/3, -1/6, 2/3, -1/6],[-1/6, -1/3, -1/6, 2/3]])
-v = np.zeros((9,1))
+    Returns:
+        gradJv (np.ndarray): A column vector of size(n) containing the derivative of the cost function with respect to the fraction of metal in each of the n elements.
 
-cornerDict = {
-    1: [1,2,4,5],
-    2: [2,3,5,6],
-    3: [4,5,7,8],
-    4: [5,6,8,9],
-}
+    """
+    elements = T.size
+    gradJv = np.zeros((elements,1))
+    km = 65
+    kp = 0.2
+    K0 = np.matrix([[2/3, -1/6, -1/3, -1/6],[-1/6, 2/3, -1/6, -1/3],[-1/3, -1/6, 2/3, -1/6],[-1/6, -1/3, -1/6, 2/3]])
 
-def adjoint(K,F,v):
-    gradJv = np.zeros((e,1))
-
-    T = np.linalg.solve(K,F)
-
-    for i in range(e):
-        idx = i + 1
-        corners = cornerDict.get(idx)
-        Te = np.array([corners]).T
-        gradJv[i] = - 0.5 * p * v[i] ** (p - 1) * (km - kp) * Te.T @ K0 @ Te
+    for el in range(elements):
+        elementVertices = corners.get(el)
+        Te = np.take(T, elementVertices)
+        gradJv[el] = - 0.5 * p * (v[el] ** (p - 1)) * (km - kp) * Te.T @ K0 @ Te
 
     return gradJv
-    
-grad = adjoint(K,F,v)
-print(grad)
-
