dad40547ee06922d612ec3e2af5ee6a44856a874
JVerhe <verherstraeten.jens@gmail.com>
2025-02-21 16:36:15 +0100

    added eigen to project

--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@
 - proto: contains the python prototype of the project
 - src: contains the files for the C++ project
 - test: contains automatic unit tests
+
+To compile the project:
+
+1. cd build
+2. cmake ..
+3. cmake --build . (everytime you want to compile the project)
+
+Compilation flags can be changed in CMakeLists.txt
--- src/adjointMethod.cpp
+++ src/adjointMethod.cpp
@@ -1,6 +1,15 @@
-#include "iostream"
+#include <iostream>
+#include <Eigen/Dense>  // Include the dense matrix module
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Eigen::Matrix2d mat;  // 2x2 matrix of doubles
+    mat << 1, 2,
+        3, 4;  // Initializing the matrix
+
+    Eigen::Vector2d vec(5, 6);  // 2D vector
+
+    Eigen::Vector2d result = mat * vec;  // Matrix-vector multiplication
+
+    std::cout << "Result:\n" << result << std::endl;
     return 0;
 }
