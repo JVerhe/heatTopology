bfc68022b01a03a23a8e73b56f9a09edb52fe213
Andries Macken <andries.macken@student.kuleuven.be>
2025-02-24 12:53:27 +0100

    debug of optimization() and cleaning of main file. also localized the functions (no use of global variables andymore)

---
+++ proto/helper_functions.py
@@ -0,0 +1,230 @@
+import numpy as np
+from adjointMethod import adjoint
+
+def create_rectangle_and_mesh(number_of_points): 
+    
+    mesh = np.zeros((number_of_points,number_of_points))
+    count = 0
+    for i in range(number_of_points):
+        for j in range(number_of_points):
+           mesh[i,j] = count ; count+=1 
+           
+    rectangles = []
+    for i in range(1,number_of_points):
+        for j in range(0,number_of_points-1):
+            line = []
+            line.append(mesh[i][j])
+            line.append(mesh[i][j+1])
+            line.append(mesh[i-1][j+1])
+            line.append(mesh[i-1][j])
+            rectangles.append(line)
+    
+    #print(mesh) ; print(rectangles)
+    return rectangles
+
+
+
+def create_coordinates(L,number_of_points):
+    
+    coordinates=np.zeros((number_of_points*number_of_points,2))
+    h = L/(number_of_points-1)
+    for i in range(number_of_points):
+        for j in range(number_of_points):
+            coordinates[i*number_of_points+j][0] = j*h
+            coordinates[i*number_of_points+j][1] = i*h
+    
+    return coordinates
+
+
+def filter_boundary_points_with_index(coordinates, L, lower=0.003, upper=0.007):
+
+    boundary_points = []
+
+    for idx, point in enumerate(coordinates):
+        x, y = point
+        if ((x == 0 or x == L) and lower <= y <= upper):
+            boundary_points.append([idx, x, y])  # Stocke (index, x, y)
+
+    return boundary_points
+        
+        
+def fill_in_k(v,k_max,k_min,p): 
+    k=np.zeros(len(v))
+    for i in range(len(v)):
+        k[i] = k_min +(k_max-k_min)*(v[i]**p)
+    
+    return k
+    
+
+def find_K(v,rectangles,number_of_points,local_matrix,k_min,k_max,penal):
+    
+    K = np.zeros((number_of_points*number_of_points,number_of_points*number_of_points))
+    k_values = fill_in_k(v,k_max,k_min,p=penal)
+    for e,rectangle in enumerate(rectangles):
+        k_e = k_values[e]  
+        for l in range(4):
+            for m in range(4):
+                index_i = int(rectangle[l])
+                index_j = int(rectangle[m])
+                value   = local_matrix[l][m]
+                K[index_i,index_j] += k_e*value
+                
+    return K
+
+
+def find_F(rectangles,number_of_points,L):
+    h = L/(number_of_points-1)
+    F = np.zeros(number_of_points * number_of_points)
+
+    for rectangle in rectangles: 
+        F_local = (h**2)*(10**7) / (4)
+    
+        for l in range(4):
+            index_i = int(rectangle[l])  
+            F[index_i] += F_local 
+    
+    return F
+            
+
+def apply_boundary(K,F,boundary_points,T_k):
+    for point in boundary_points : 
+        index_of_point = point[0]
+        F = F - T_k*K[:,index_of_point]
+        for i in range(len(K)):
+            K[i,index_of_point]=0
+            K[index_of_point,i]=0
+        K[index_of_point,index_of_point] =1
+        F[index_of_point] = T_k
+    
+    return K,F
+
+
+def objective(v,rectangles,T,K0,k_min,k_max,p):
+    D = 0 
+    k_values = fill_in_k(v,k_max,k_min,p)
+    for e,rectangle in enumerate(rectangles):
+        k_e = k_values[e]  
+        T_loc = np.zeros((4,1))
+        for l in range(4):
+            T_loc[l][0]=T[int(rectangle[l])-1]
+        
+        K = np.array(K0)*k_e
+        D+= ((T_loc.T)@K@T_loc)[0][0]
+          
+    return D
+
+
+def optimization(K0,F,max_vol_frac,nx,ny,penal,rectangles,L,boundary_temp,ft):
+    """
+    Perform heat topology optimization with volume constraints via heuristic algorithm.
+    Sensitivity or density filtering can be applied. 
+    
+    Args:
+        K0 (np.ndarray): stiffness matrix of size 4x4
+        F (np.ndarray): Right-hand-side vector for solving linear system. 
+        max_vol_frac (float): maximum volume fraction metal/plastic
+        nx (int): amount of elements in x-direction
+        ny (int): amount of elements in y-direction
+        penal (float): (exponent) penalization for constructing stiffness matrix.
+                       Typically, penal=3
+                        E_e = E_min + x_e^penal * (E_0 - E_min)
+         rectangles(nested list): 
+            Nested list with first index the elements and second index the nodes of said element
+        L (float): square side length
+        boundary_temp (float): fixed temperature at certain boundaries
+        ft (int): if 0: no filtering
+                  if 1: sensitivity filtering
+                  if 2: density filtering
+       
+    
+    Returns: an optimal vector x containing the volume fractions
+    """
+    #Total elements is nx*ny, total grid points is (nx+1)*(ny+1)
+    assert(nx==ny)
+    #define problem-specific parameters
+    E_min = 0.2
+    E_0 = 65
+    rmin = 0.04*nx
+    
+    # number_of_points = int(np.sqrt(nx) +1) why??
+    N_total_elements = nx*ny
+    N_total_points = (nx+1)*(ny+1)
+    N_points_1D = nx+1
+
+    #Prepare filter
+    #iH = np.ones((nx*ny*(2*(ceil(rmin)-1)+1)**2,1))
+    #jH = np.ones(np.shape(iH))
+    #sH = np.zeros(np.shape(iH))
+    #H = sH 
+    #H = coo_matrix(sH, (iH, jH))
+    #Hs =  np.sum(H,axis=1)
+    #TODO: Prepare filter further
+
+    #prepare for optimization iterations
+    #x = np.tile(max_vol_frac,(ny,nx))
+    #x = x.flatten(order='F') #needed? from matrix to vector
+    x = np.ones(nx*ny)*max_vol_frac
+    xPhys = x
+    loop = 0
+    change = 0.1
+    coordinates = create_coordinates(L,N_points_1D)
+    boundary_points=filter_boundary_points_with_index(coordinates,L)
+    #start iteration
+    while change > 0.01:
+        loop += 1
+        #FE-analysis
+        K = find_K(xPhys,rectangles,N_points_1D,K0,E_min,E_0,penal)
+        F = find_F(rectangles,N_points_1D,L)
+        K,F = apply_boundary(K,F,boundary_points,boundary_temp)
+        # print(K.shape) ; print(F.shape)
+        U = np.linalg.solve(K,F)
+        
+        #Objective function and sensitivity analysis
+        c = objective(xPhys,rectangles,U,K0,E_min,E_0,penal)
+        dc = adjoint(U,xPhys,rectangles,K0,p=penal).ravel() #sensitivity of objective
+        assert(np.shape(dc) == np.shape(xPhys))
+        dv = np.ones(len(dc))# Sensitivity of volume constraint
+        #dv = dv.flatten(order='F')#from matrix to vector, is this okay?
+
+        #Filtering/Modification of sensitivities
+        """
+        if ft==1: #sensitivity filtering
+            dc = (H @ (x.ravel(order='F') * dc.ravel(order='F'))) / Hs / np.maximum(1e-3, x.ravel(order='F'))
+        elif ft==2: #density filtering
+            dc = H@(dc.ravel(order='F')/Hs)
+            dv = H@(dv.ravel(order='F')/Hs)
+        #else (ft==0): continue
+        """
+
+        #optimality criteria update of design variables and physical densities
+        l1=0
+        l2=1e9
+        move = 0.2 #m in paper
+        while (l2-l1)/(l1+l2) > 1e-3:
+            lmid = 0.5*(l1+l2)
+            #print("l2=",l2)
+            B = np.sqrt(np.maximum(-dc/(dv*lmid),0)) #neg. values close to e_mach do not have np.sqrt()
+            xnew = np.maximum(0,np.maximum(x-move,np.minimum(x+move,x*B)))
+            #print("npmin",np.minimum(x+move,x*B))
+            xPhys = xnew
+            if np.sum(xPhys) > max_vol_frac*nx*nx:
+                #print("np.sum(xPhys) too big:",np.sum(xPhys))
+                l1 = lmid 
+            else: l2 = lmid
+            """
+            if ft == 1 or ft == 0:
+                xPhys = xnew
+            elif ft == 2:
+                xPhys = (H@xnew.ravel(order='F'))/Hs
+            if np.sum(xPhys) > max_vol_frac*(nx):
+                l1 = lmid
+            else: l2 = lmid
+            """
+        
+        #print(xPhys)
+        change = np.max(np.abs(xnew-x))
+        x = xnew
+        #print results
+        print(f" It.: {loop} Obj.: {c} Vol.: {np.mean(xPhys)} ch.: {change}")
+        # Plot density distribution
+    return x
--- proto/main.py
+++ proto/main.py
@@ -1,156 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse import csr_matrix
-from scipy.optimize import minimize
-import numpy as np
-from scipy.sparse import coo_matrix, csr_matrix 
 import matplotlib.pyplot as plt
 from math import ceil, sqrt
-
+from helper_functions import *
 
 
 L = 0.01 
 outlet = 0.004    
-number_of_points =10; h = L/(number_of_points-1)
+number_of_points =10
 number_of_rectangle = (number_of_points-1)*(number_of_points-1)
-p = 2
+p = 3
 k_min = 0.2
 k_max = 65
 T_k =  293
 
 local_matrix = [[2/3,-1/6,-1/3,-1/6],[-1/6,2/3,-1/6,-1/3],[-1/3,-1/6,2/3,-1/6],[-1/6,-1/3,-1/6,2/3]]
 
-
-def create_rectangle_and_mesh(number_of_points): 
-    
-    mesh = np.zeros((number_of_points,number_of_points))
-    count = 0
-    for i in range(number_of_points):
-        for j in range(number_of_points):
-           mesh[i,j] = count ; count+=1 
-           
-    rectangles = []
-    for i in range(1,number_of_points):
-        for j in range(0,number_of_points-1):
-            line = []
-            line.append(mesh[i][j])
-            line.append(mesh[i][j+1])
-            line.append(mesh[i-1][j+1])
-            line.append(mesh[i-1][j])
-            rectangles.append(line)
-    
-    #print(mesh) ; print(rectangles)
-    return rectangles
-
-
-
-def create_coordinates(L,number_of_points):
-    
-    coordinates=np.zeros((number_of_points*number_of_points,2))
-    h = L/(number_of_points-1)
-    for i in range(number_of_points):
-        for j in range(number_of_points):
-            coordinates[i*number_of_points+j][0] = j*h
-            coordinates[i*number_of_points+j][1] = i*h
-    
-    return coordinates
-
-
-def filter_boundary_points_with_index(coordinates, L, number_of_points, lower=0.003, upper=0.007):
-
-    boundary_points = []
-
-    for idx, point in enumerate(coordinates):
-        x, y = point
-        if ((x == 0 or x == L) and lower <= y <= upper):
-            boundary_points.append([idx, x, y])  # Stocke (index, x, y)
-
-    return boundary_points
-        
-        
-def fill_in_k(v,k_max,k_min,p): 
-    k=np.zeros(len(v))
-    for i in range(len(v)):
-        k[i] = k_min +(k_max-k_min)*(v[i]**p)
-    
-    return k
-        
-
 rectangles = create_rectangle_and_mesh(number_of_points)
 coordinates = create_coordinates(L,number_of_points)
-boundary_points = filter_boundary_points_with_index(coordinates,0.01,number_of_points)
-
-    
-def find_K(v,number_of_points):
-    
-    K = np.zeros((number_of_points*number_of_points,number_of_points*number_of_points))
-    fill_in_k(v,k_max,k_min,2)
-    for e,rectangle in enumerate(rectangles):
-        k_e = k_values[e]  
-        for l in range(4):
-            for m in range(4):
-                index_i = int(rectangle[l])
-                index_j = int(rectangle[m])
-                value   = local_matrix[l][m]
-                K[index_i,index_j] += k_e*value
-                
-    return K
+boundary_points = filter_boundary_points_with_index(coordinates,L)
 
 
-def find_F():
-    
-    F = np.zeros(number_of_points * number_of_points)
+v = np.ones(number_of_rectangle)*0.2
+k_values = fill_in_k(v,65,0.2,p)
+K = find_K(v,rectangles,number_of_points,local_matrix,k_min,k_max,p)
+F = find_F(rectangles,number_of_points,L)
+K,F = apply_boundary(K,F,boundary_points,T_k) ; T = np.linalg.solve(K,F)  
 
-    for rectangle in rectangles: 
-        F_local = (h**2)*(10**7) / (4)
-    
-        for l in range(4):
-            index_i = int(rectangle[l])  
-            F[index_i] += F_local 
-    
-    return F
-            
-
-def apply_boundary(K,F):
-    
-    for point in boundary_points : 
-        index_of_point = point[0]
-        F = F - T_k*K[:,index_of_point]
-        for i in range(len(K)):
-            K[i,index_of_point]=0
-            K[index_of_point,i]=0
-        K[index_of_point,index_of_point] =1
-        F[index_of_point] = T_k
-    
-    return K,F
-    
-            
-            
-v = np.ones(number_of_rectangle)*0.2 ; k_values = fill_in_k(v,65,0.2,p)
-K = find_K(v,number_of_points)
-F = find_F()
-K,F = apply_boundary(K,F) ; T = np.linalg.solve(K,F)  
 
 #################################################### Optimisation ###################################################
 
 
-def f(v):
-    D = 0 
-    k_values = fill_in_k(v,k_max,k_min,p)
-    for e,rectangle in enumerate(rectangles):
-        k_e = k_values[e]  
-        T_loc = np.zeros((4,1))
-        for l in range(4):
-            T_loc[l][0]=T[int(rectangle[l])-1]
-        
-        K = np.array(local_matrix)*k_e
-        D+= ((T_loc.T)@K@T_loc)[0][0]
-          
-    return D
+x = optimization(
+    K0=local_matrix,
+    F=F,
+    max_vol_frac=0.4,
+    nx=number_of_points-1,
+    ny=number_of_points-1,
+    penal=p,
+    rectangles=rectangles,
+    L=L,
+    boundary_temp=T_k,
+    ft=0,
+)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 def grad_f(v):
+    """
+    This function accomplishes the same as adjointMethod and is therefore redundant.
+    """
     gradient_D = np.zeros(number_of_rectangle)
     k_values = fill_in_k(v,k_max,k_min,p)
     for e,rectangle in enumerate(rectangles):
@@ -163,121 +74,6 @@ def grad_f(v):
         gradient_D[e] = ((-1/2)*p*((v[e])**(p-1))*(k_max-k_min)*((T_loc.T)@K@T_loc)*(1/k_e))[0][0]
         
     return gradient_D
-    
-
-
-
-
-def optimization(K0,F,max_vol_frac,nx,ny,penal,corners,ft):
-    """
-    Perform heat topology optimization with volume constraints via heuristic algorithm.
-    Sensitivity or density filtering can be applied. 
-    
-    Args:
-        K0 (np.ndarray): stiffness matrix of size 4x4
-        F (np.ndarray): Right-hand-side vector for solving linear system. 
-        max_vol_frac (float): maximum volume fraction metal/plastic
-        nx (int): amount of elements in x-direction
-        ny (int): amount of elements in y-direction
-        penal (float): (exponent) penalization for constructing stiffness matrix.
-                       Typically, penal=3
-                        E_e = E_min + x_e^penal * (E_0 - E_min)
-         corners(nested list): 
-            Nested list with first index the elements and second index the nodes of said element
-        ft (int): if 0: no filtering
-                  if 1: sensitivity filtering
-                  if 2: density filtering
-       
-    
-    Returns: an optimal vector x containing the volume fractions
-    """
-    #Total elements is nx*ny, total grid points is (nx+1)*(ny+1)
-
-    #define problem-specific parameters
-    Emin = 0.2
-    E0 = 65
-    rmin = 0.04*nx
-    
-    number_of_points = int(np.sqrt(nx) +1)
-
-    #Prepare filter
-    #iH = np.ones((nx*ny*(2*(ceil(rmin)-1)+1)**2,1))
-    #jH = np.ones(np.shape(iH))
-    #sH = np.zeros(np.shape(iH))
-    #H = sH 
-    #H = coo_matrix(sH, (iH, jH))
-    #Hs =  np.sum(H,axis=1)
-    #TODO: Prepare filter further
-
-    #prepare for optimization iterations
-    #x = np.tile(max_vol_frac,(ny,nx))
-    #x = x.flatten(order='F') #needed? from matrix to vector
-    x = np.ones(nx)*0.2
-    xPhys = x
-    loop = 0
-    change = 0.1
-    #start iteration
-    while change > 0.01:
-        loop += 1
-        #FE-analysis
-        K = find_K(xPhys,number_of_points) ; F=find_F()
-        K,F = apply_boundary(K,F)
-        print(K.shape) ; print(F.shape)
-        U = np.linalg.solve(K,F)
-        
-        #Objective function and sensitivity analysis
-        c = f(xPhys)
-        dc = grad_f(xPhys) #sensitivity of objective
-        dv = np.ones(len(dc))# Sensitivity of volume constraint
-        #dv = dv.flatten(order='F')#from matrix to vector, is this okay?
-
-        #Filtering/Modification of sensitivities
-        if ft==1:
-            dc = (H @ (x.ravel(order='F') * dc.ravel(order='F'))) / Hs / np.maximum(1e-3, x.ravel(order='F'))
-        elif ft==2:
-            dc = H@(dc.ravel(order='F')/Hs)
-            dv = H@(dv.ravel(order='F')/Hs)
-        #else (ft==0): continue
-
-        #optimality criteria update of design variables and physical densities
-        l1=0
-        l2=1e9
-        move = 0.1 #m in paper
-        count = 0 
-        while (l2-l1)/(l1+l2) > 1e-3 and count < 1000:
-            count = count +1
-            lmid = 0.5*(l1+l2)
-            print(l2)
-            B = np.zeros(len(dc))
-            for i in range(len(B)):
-                if dv[i] !=0:
-                    B[i] = np.sqrt(-dc[i]/(dv[i]*lmid))
-                else : 
-                    B[i] = 0
-                
-            xnew = np.maximum(0,np.maximum(x-move,np.minimum(x+move,x*B)))
-            if ft == 1 or ft == 0:
-                xPhys = xnew
-            elif ft == 2:
-                xPhys = (H@xnew.ravel(order='F'))/Hs
-            if np.sum(xPhys) > max_vol_frac*(nx):
-                l1 = lmid
-            else: l2 = lmid
-            
-            print(np.sum(xPhys),max_vol_frac*(nx))
-            print(xPhys)
-        change = np.max(np.abs(xnew-x))
-        x = xnew
-        #print results
-        print(f" It.: {loop:5d} Obj.: {c:11.4f} Vol.: {np.mean(xPhys):7.3f} ch.: {change:7.3f}")
-        # Plot density distribution
-    return x
-
-
-
-
-x = optimization(local_matrix,F,0.4,number_of_rectangle,number_of_rectangle,p,rectangles,0)
-
 
 
 # T_matrix = T.reshape((number_of_points, number_of_points))
