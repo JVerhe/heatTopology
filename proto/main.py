b37dce6012f3fdfb45b1d344d169e30f1b76d334
JVerhe <verherstraeten.jens@gmail.com>
2025-02-24 17:14:32 +0100

    made changes to main

--- proto/main.py
+++ proto/main.py
@@ -3,12 +3,17 @@ import matplotlib.pyplot as plt
 from math import sqrt
 from meshHelper import *
 from optimization import optimize
+import sys
 
+try:
+    number_of_points = int(sys.argv[1])
+except:
+    print("You must give a number of discretization points when executing this file: ex. python3 main.py 30")
+    exit()
 
 #################################################### Discretisation ###################################################
 
 L = 0.01 
-number_of_points = 15
 p = 3
 T_k =  293
 local_matrix = [[2/3,-1/6,-1/3,-1/6],[-1/6,2/3,-1/6,-1/3],[-1/3,-1/6,2/3,-1/6],[-1/6,-1/3,-1/6,2/3]]
@@ -16,7 +21,6 @@ rectangles = create_rectangle_and_mesh(number_of_points)
 coordinates = create_coordinates(L,number_of_points)
 boundary_points = filter_boundary_points_with_index(coordinates,L)
 F = find_F(rectangles,number_of_points,L)
-
 
 #################################################### Optimisation ###################################################
 
