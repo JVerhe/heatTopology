826bd1a4aba9330c9f32be0807d26a70aeecc483
Andries Macken <andries.macken@student.kuleuven.be>
2025-03-05 15:07:50 +0100

    first test

--- test/test_optimization.cpp
+++ test/test_optimization.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <cassert>
 
 // Include the next 5 lines in your own files
 #include <testFramework.hpp>
-#include <../src/meshHelper.hpp>
+#include <../src/optHelper.hpp>
 #define check(cond) check(cond, __LINE__, __FILE__)
 #define compare(val1, val2) compare(val1, val2, __LINE__, __FILE__)
 #define compareTolerance(val1, val2, tol) compareTolerance(val1, val2, tol, __LINE__, __FILE__)
 
-void exampleTest1() { // Should work
+using namespace Eigen;
+
+void test_fill_in_k() {
+    VectorXd v = VectorXd::Zero(10);
+    VectorXd k = VectorXd::Zero(v.size());
+    for (size_t i=0;i<v.size();i++) v[i] = i/v.size();
+    int p = 1; //eys
+    double k_min = 0; 
+    double k_max = 1;
+    fill_in_k(k,v,k_max,k_min,p);
+    tf::compare(true,v.isApprox(k));
+    
+
     tf::check(true);
     tf::check(1); // 1 is equal to true in c++
     tf::compare(1, 1); // Arguments should be of the exact same type
@@ -21,12 +34,9 @@ void exampleTest1() { // Should work
 
     tf::compare(true, v1.isApprox(v2));
     tf::compareTolerance(0., (v1 - v2).norm(), 0.1);
-
-    create_rectangle_and_mesh(50); // Functions of the project can still be called
 }
 
 int main() {
-    exampleTest1();
+    test_fill_in_k();
     return 0;
 }
-
